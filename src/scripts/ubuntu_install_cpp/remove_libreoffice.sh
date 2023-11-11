4b4030c956f9b475f0d089937985b9ff78f8df77
Calfmark <fucefu@163.com>
2023-11-11 18:12:14 +0800

    add fiel

---
+++ src/scripts/ubuntu_install_cpp/remove_libreoffice.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+sudo apt remove -y libreoffice-calc
+
+
+sudo apt remove -y libreoffice-draw
+
+
+sudo apt remove -y libreoffice-impress
+
+
+sudo apt remove -y libreoffice-writer
+
+
+sudo apt remove -y libreoffice*
+
+
+sudo apt purge -y libreoffice*
+
+sudo apt autoremove -y
