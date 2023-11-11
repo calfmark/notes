21fe4da0bf05c85bddc7f781a4af6399c5f13892
calfmark <fufuzai@gmail.com>
2023-11-11 22:32:35 +0800

    delete unuse file

--- src/scripts/ubuntu_install_cpp/install_c  _env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt install -y build-essential gdb
-sudo apt install -y cmake
-sudo apt install -y libgtest-dev
-sudo apt install -y libudev-dev
-sudo apt install -y libblkid-dev
-sudo apt install -y libaio-dev
-sudo apt install -y libuv1-dev
-sudo apt install -y gcovr
